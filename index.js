5e003df1cdb41ad042cce26098471fbe8aeef379
Sohan Millat Sakib <millatsakib05@gmail.com>
2024-07-08 13:55:02 +0600

    Schema added

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.use(express.json());
 
 mongoose.connect(process.env.MONGODB_URI, {
     useNewUrlParer: true,
-    useUnifiedTopology: ture
+    useUnifiedTopology: true
 })
     .then(() => console.log("Connected to MongoDB"))
     .catch(err => console.log("Failed to connect MongoDB", err));
